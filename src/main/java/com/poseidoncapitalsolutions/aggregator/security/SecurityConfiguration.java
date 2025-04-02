026607f13620a9260f87ee54e1de2585dc7b6f4a
IzawGithub <IzawGithub@pm.me>
2025-04-02 12:15:08 +0200

    feat(Security): Configure form based authentication

--- src/main/java/com/poseidoncapitalsolutions/aggregator/security/SecurityConfiguration.java
+++ src/main/java/com/poseidoncapitalsolutions/aggregator/security/SecurityConfiguration.java
@@ -2,17 +2,53 @@ package com.poseidoncapitalsolutions.aggregator.security;
 
 import com.poseidoncapitalsolutions.aggregator.utils.Password;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfiguration {
+    /**
+     * @see
+     *     https://docs.spring.io/spring-security/reference/servlet/configuration/java.html#jc-httpsecurity
+     */
+    @Bean
+    @NonNull SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
+                // We use standard form based auth
+                .formLogin(form -> form.loginPage("/auth/log-in")
+                        .permitAll())
+                .logout(logout -> logout.logoutUrl("/auth/log-out")
+                        .logoutSuccessUrl("/")
+                        .addLogoutHandler(new HeaderWriterLogoutHandler(
+                                new ClearSiteDataHeaderWriter(Directive.ALL)))
+                        .permitAll())
+                .csrf(Customizer.withDefaults())
+                .rememberMe(Customizer.withDefaults())
+                .build();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder builder) {
+        builder.eraseCredentials(false);
+    }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         final var parameters = Password.builder();
