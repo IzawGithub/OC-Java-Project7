eb897df7e39a452e07d3e40757284c59c0720108
IzawGithub <IzawGithub@pm.me>
2025-04-02 11:45:53 +0200

    feat(Domain): Create `User` class

---
+++ src/main/java/com/poseidoncapitalsolutions/aggregator/domains/User.java
@@ -0,0 +1,35 @@
+package com.poseidoncapitalsolutions.aggregator.domains;
+
+import com.poseidoncapitalsolutions.aggregator.domains.internal.Password;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.springframework.lang.NonNull;
+
+import java.util.UUID;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+    @Builder.Default
+    @Id
+    @NonNull private UUID id = UUID.randomUUID();
+
+    private String username;
+
+    @Embedded
+    private Password password;
+
+    private String fullname;
+    private String role;
+}
