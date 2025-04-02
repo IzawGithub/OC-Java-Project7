410f8be4befddb073a76a89c32ebaa635c81144c
IzawGithub <IzawGithub@pm.me>
2025-04-02 11:49:46 +0200

    feat(Service): Create `GenericService` class
    This define a class that can be implemented for any `T` model, and give
    access to its fundamental CRUD operation.

---
+++ src/main/java/com/poseidoncapitalsolutions/aggregator/services/GenericService.java
@@ -0,0 +1,54 @@
+package com.poseidoncapitalsolutions.aggregator.services;
+
+import com.poseidoncapitalsolutions.aggregator.domains.helper.IDomain;
+
+import jakarta.transaction.Transactional;
+
+import net.xyzsd.dichotomy.Maybe;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+import java.util.UUID;
+
+public class GenericService<T extends IDomain, Repository extends JpaRepository<T, UUID>> {
+    // -- CRUD --
+
+    @Transactional
+    public void create(@NonNull final T item) {
+        repository.save(item);
+    }
+
+    public List<T> readAll() {
+        return repository.findAll();
+    }
+
+    public Maybe<T> maybeRead(@NonNull final T item) {
+        return maybeReadById(item.getId());
+    }
+
+    public Maybe<T> maybeReadById(@NonNull final UUID id) {
+        return Maybe.ofNullable(repository.findById(id).orElseGet(() -> null));
+    }
+
+    @Transactional
+    public void update(@NonNull final T item) {
+        var itemDb = repository.getReferenceById(item.getId());
+        itemDb = item;
+        repository.save(itemDb);
+    }
+
+    @Transactional
+    public void delete(@NonNull final UUID id) {
+        repository.deleteById(id);
+    }
+
+    // -- Beans --
+
+    protected final @NonNull Repository repository;
+
+    public GenericService(@NonNull final Repository genericRepository) {
+        this.repository = genericRepository;
+    }
+}
