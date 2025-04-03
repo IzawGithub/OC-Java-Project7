e50e3a7932405b9222a55a7745836768708192d3
IzawGithub <IzawGithub@pm.me>
2025-04-03 15:51:58 +0200

    feat(Controller): Add `HomeController` class

---
+++ src/main/java/com/poseidoncapitalsolutions/aggregator/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.poseidoncapitalsolutions.aggregator.controller;
+
+import net.xyzsd.dichotomy.Maybe;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@PreAuthorize("permitAll()")
+public class HomeController {
+    @GetMapping
+    public @NonNull ModelAndView home(@AuthenticationPrincipal final UserDetails userAuth) {
+        return Maybe.ofNullable(userAuth)
+                .map(unnamned -> new ModelAndView("redirect:/bidList"))
+                .orElse(new ModelAndView("home"));
+    }
+}
