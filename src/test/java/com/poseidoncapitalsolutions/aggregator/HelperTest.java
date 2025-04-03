e913a1c0661f7302ea2a462aea7e28a97dcf3e1f
IzawGithub <IzawGithub@pm.me>
2025-04-03 14:57:56 +0200

    test(Fixture): Add helper for frontend controller

--- src/test/java/com/poseidoncapitalsolutions/aggregator/HelperTest.java
+++ src/test/java/com/poseidoncapitalsolutions/aggregator/HelperTest.java
@@ -1,3 +1,81 @@
 package com.poseidoncapitalsolutions.aggregator;
 
-public final class HelperTest {}
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.poseidoncapitalsolutions.aggregator.domains.User;
+import com.poseidoncapitalsolutions.aggregator.utils.Password;
+
+import lombok.SneakyThrows;
+
+import net.xyzsd.dichotomy.Maybe;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+public final class HelperTest {
+    private static final Pattern REGEX_CSRF =
+            Pattern.compile("(?<begin><input .* name=\"_csrf\" value=\")(?<value>.*)(?<end>\"/>)");
+    private static final Pattern REGEX_ARGON2 = Pattern.compile("\\$argon2.*");
+
+    public static final String JOHN_DOE_PASSWORD = "Val1dPassword!";
+
+    public static UUID uuid() {
+        return UUID.fromString("76543210-dcba-cba1-dcba-ba9876543210");
+    }
+
+    @SneakyThrows
+    public static User johnDoe() {
+        return User.builder()
+                .id(UUID.fromString("01234567-abcd-1abc-abcd-0123456789ab"))
+                .username("John Doe")
+                .password(Password.builder().secret(JOHN_DOE_PASSWORD).build())
+                .fullname("FullName John Doe")
+                .role("USER")
+                .build();
+    }
+
+    public static String sanitizedHtml(@NonNull final String html) {
+        var sanitisedHtml = html;
+        final var matcherCsrf = REGEX_CSRF.matcher(sanitisedHtml);
+        if (matcherCsrf.find()) {
+            sanitisedHtml = matcherCsrf.replaceAll("${begin}${end}");
+        }
+        final var matcherArgon2 = REGEX_ARGON2.matcher(sanitisedHtml);
+        if (matcherArgon2.find()) {
+            sanitisedHtml = matcherArgon2.replaceAll("");
+        }
+        return sanitisedHtml;
+    }
+
+    public static Maybe<User> expectAuth(
+            @NonNull final ResultActions mvc, @NonNull final String redirectUrl) throws Exception {
+        return Maybe.ofNullable(
+                        (SecurityContextImpl)
+                                andExpectRedirect(mvc, redirectUrl)
+                                        .andReturn()
+                                        .getRequest()
+                                        .getAttribute(
+                                                "org.springframework.security.web.context.RequestAttributeSecurityContextRepository.SPRING_SECURITY_CONTEXT"))
+                .flatMap(securityContext -> Maybe.ofNullable(securityContext.getAuthentication()))
+                .map(Authentication::getPrincipal)
+                .map(User.class::cast);
+    }
+
+    // -- lombok.ExtensionMethod --
+
+    public static ResultActions andExpectRedirectAuth(@NonNull final ResultActions mvc)
+            throws Exception {
+        return andExpectRedirect(mvc, "http://localhost/auth/log-in");
+    }
+
+    public static ResultActions andExpectRedirect(
+            @NonNull final ResultActions mvc, @NonNull final String redirectUrl) throws Exception {
+        return mvc.andExpectAll(status().isFound(), redirectedUrl(redirectUrl));
+    }
+}
